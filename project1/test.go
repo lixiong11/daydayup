cfa7bb6fa3284d5d41bc526bf6dd2ca4e8c5b45c
lixiong11 <18566682196@163.com>
2025-02-20 19:44:56 +0800

    增加project1文件夹路径

---
+++ project1/test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "net/http"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
+}
+
+func main_one() {
+	//1.创建gin对象
+	r := gin.Default()
+	r.Delims("{[{", "}]}")
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	r.LoadHTMLFiles("./templates/raw.tmpl")
+	r.GET("/raw", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "raw.tmpl", map[string]interface{}{
+			"now": time.Date(2017, 07, 01, 0, 0, 0, 0, time.UTC),
+		})
+	})
+	r.Run(":8080")
+
+}

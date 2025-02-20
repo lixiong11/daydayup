091285ad7804d4c3d851dbc432eed4bb162caaa3
lixiong11 <18566682196@163.com>
2025-02-20 20:51:55 +0800

    修改project1项目文件

--- project1/test.go
+++ project1/test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "net/http"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -10,19 +9,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 自定义模板功能
 func formatAsDate(t time.Time) string {
 	year, month, day := t.Date()
 	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
 }
 
-func main_one() {
+func main() {
 	//1.创建gin对象
 	r := gin.Default()
 	r.Delims("{[{", "}]}")
 	r.SetFuncMap(template.FuncMap{
 		"formatAsDate": formatAsDate,
 	})
-	r.LoadHTMLFiles("./templates/raw.tmpl")
+	r.LoadHTMLFiles("./testdata/template/raw.tmpl")
 	r.GET("/raw", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "raw.tmpl", map[string]interface{}{
 			"now": time.Date(2017, 07, 01, 0, 0, 0, 0, time.UTC),

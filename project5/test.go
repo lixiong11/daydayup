c82c94e3f13700baef1eaff518a33e4a7ef11247
lixiong11 <18566682196@163.com>
2025-02-20 20:27:38 +0800

    增加project5项目文件

--- project5/test.go
+++ project5/test.go
@@ -10,12 +10,12 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/**/*")
 	router.GET("posts/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Posts",
 		})
 	})
 	router.GET("/users/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Users",
 		})
 	})

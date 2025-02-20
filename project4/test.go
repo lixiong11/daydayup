b55c305b48ee10eb31501ca90a38c85c24aa2455
lixiong11 <18566682196@163.com>
2025-02-20 20:14:25 +0800

    增加project4项目文件备注

--- project4/test.go
+++ project4/test.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
+	//创建路由对象
 	router := gin.Default()
+	//使用LoadHTMLGlob()或者LoadHTMLFiles()
+	// templates目录下的index.tmpl文件
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			//访问提示页面
 			"title": "Main website",
 		})
 	})
+	//访问地址：http://127.0.0.1:8080/index
 	router.Run(":8080")
 }

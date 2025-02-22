731c7012703bacc58950f4c0eca2956b013128b1
lixiong11 <18566682196@163.com>
2025-02-22 15:48:55 +0800

    修改project10项目文件

--- project10/test.go
+++ project10/test.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	router := gin.Default()
 	router.POST("/form_post", func(c *gin.Context) {
+		//c.PostForm("message"):从POSt请求的表单数据中获取message的字段值如果字段不存在返回为空
 		message := c.PostForm("message")
+		//c.DefaultPostForm("nick", "tony")：从POST请求的表单数中获取nick的值，若字段不存在返回空字符串
 		nick := c.DefaultPostForm("nick", "tony")
 		c.JSON(200, gin.H{
 			"status":  "posted",

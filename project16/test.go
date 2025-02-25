683790a2dc480efd7abaccc47586dbd5e0173dc4
lixiong11 <18566682196@163.com>
2025-02-25 18:40:47 +0800

    增加project20项目文件

---
+++ project16/test.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type myForm struct {
+	Colors []string `form:"colors[]"`
+}
+
+func formHandler(c *gin.Context) {
+	var fakeForm myForm
+	c.ShouldBind(&fakeForm)
+	c.JSON(200, gin.H{"color": fakeForm.Colors})
+}
+
+func main() {
+
+}

eb6da7e6f448296b5f14ee9ca18b06d37703b78e
lixiong11 <18566682196@163.com>
2025-02-27 16:31:04 +0800

    修改project22项目工程文件

--- project22/test.go
+++ project22/test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//默认路由日志格式：
+//[GIN-debug] POST   /foo                      --> main.main.func1 (3 handlers)
+//[GIN-debug] GET    /bar                      --> main.main.func2 (3 handlers)
+//[GIN-debug] GET    /status                   --> main.main.func3 (3 handlers)
+//`如果你想要以指定的格式（例如 JSON，key values 或其他格式）记录信息，则可以使用 gin.DebugPrintRouteFunc 指定格式。 在下面的示例中，我们使用标准日志包记录所有路由，但你可以使用其他满足你需求的日志工具。`
+
 func main() {
 	r := gin.Default()
 	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandler int) {

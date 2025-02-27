9d4d771ee6f1fef43c27a2e3644aefb9f989d4cc
lixiong11 <18566682196@163.com>
2025-02-27 14:55:11 +0800

    修改project23项目文件

--- project23/test.go
+++ project23/test.go
@@ -6,12 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type formA struct {
+	Foo string `josn:"foo" xml:"foo" binding:"required"`
+}
+type formB struct {
+	Bar string `json:"bar" xml:"bar" binding:"required"`
+}
+
 func SomeHandler(c *gin.Context) {
 	objA := formA{}
 	objB := formB{}
 	//c.ShouldBind使用了c.Request.Body，不可重用。
 	if errA := c.ShouldBind(&objA); errA == nil {
-		c.Sring(http.StatusOK, "the body should be formA")
-
+		c.String(http.StatusOK, "the body should be formA")
+		//因为现在c.Request.Body是EOF，所以这里会报错。
+	} else if errB := c.ShouldBind(&objB); errB == nil {
+		c.String(http.StatusOK, `the body should be formB`)
+	} else {
+		//此次省列
 	}
 }

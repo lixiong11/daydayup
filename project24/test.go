28bf6b793327aaec6b202d5fc5044c79843cafa2
lixiong11 <18566682196@163.com>
2025-02-27 14:56:44 +0800

    增加project24项目文件

---
+++ project24/test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/assets", "./assets")
+	router.StaticFS("/more_static", http.Dir("my_file_system"))
+	router.StaticFile("/favicon.ico", "./resoure/favicon.ico")
+	//监听并在0.0.0.0:8080
+	router.Run(":8080")
+}

36a08233eb32d4511d0ee33409d5cac071f8d27d
lixiong11 <18566682196@163.com>
2025-02-27 16:31:40 +0800

    修改project20项目工程文件

--- project20/test.go
+++ project20/test.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// 使用现有的基础请求对象解析查询字符串参数。
+	// 示例 URL： /welcome?firstname=Jane&lastname=Doe
 	router := gin.Default()
 	router.GET("/welcome", func(c *gin.Context) {
 		firstname := c.DefaultQuery("firstname", "Guest")
